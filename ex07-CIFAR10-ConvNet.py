75d99a1f8af43a3fe94652b61521c10c0d65a944
Chris Olah <chris@colah.ca>
2015-03-27 06:06:09 -0400

    Add CIFAR10 example.

--- ex07-CIFAR10-ConvNet.py
+++ ex07-CIFAR10-ConvNet.py
@@ -6,29 +6,24 @@ import numpy
 import vis
 
 
-mnist = data.load("mnist")
+mnist = data.load("cifar10")
 test_data = mnist["test"].values()
 train_data = mnist["train"]
 
 net = Network(
-    layers = [InputLayer([28,28]),
+    layers = [InputLayer([3,32,32]),
               ConvolutionLayer([6,6], 8, activation = ReLU),
               PoolLayer([2,2]),
               FullyConnectedLayer(100, activation = ReLU),
               SoftMaxLayer(10)],
     cost=log_likelihood)
 
-for m in range(100):
-    net.train(*train_data.get_batch(30), learning_rate = 0.01 )
-
 xs, ys = train_data.get_batch(5)
 info = net.introspect(xs, ys)
-info.keys()
 
-
-for i in range(20):
+for i in range(200):
     for m in range(1000):
-        net.train(*train_data.get_batch(30), learning_rate = 0.01 )
+        net.train(*train_data.get_batch(30), learning_rate = 0.001 )
 
     print "Training Step", i
     print "Test Accuracy:", net.accuracy(*test_data)
@@ -36,10 +31,10 @@ for i in range(20):
     print ""
 
     print "Network Visualization and Introspection:"
-    #print ""
+    print ""
     #print "Sample Inputs:"
     sample_xs, sample_ys = train_data.get_batch(30)
-    #vis.vis_matrix(sample_xs[:4], s2x2 = True)
+    #vis.vis_matrix(sample_xs[:2,0], s2x2 = True)
 
     full_info = net.introspect(sample_xs, sample_ys)
 
@@ -48,14 +43,14 @@ for i in range(20):
         info = full_info[layer_name]
         if "ConvolutionLayer" in layer_name:
             print layer_name
-            #print "Sample Activations (should be non-constant, non-blank):"
-            #F = info["y"].shape[1]
+            print "Sample Activations (should be non-constant, non-blank):"
+            F = info["y"].shape[1]
             ## Actually, let's just cycle the one we show.
             ## They're pretty messy.
-            #for fn in [i%F]:
-            #    print "Filter", fn, "Activations:"
-            #    vis.vis_matrix(info["y"][:4,fn,:22,:22], s2x2 = True)
-            #    print 80*"-"
+            for fn in [i%F]:
+                print "Filter", fn, "Activations:"
+                vis.vis_matrix(info["y"][:2,fn,:22,:22], s2x2 = True)
+                print 80*"-"
 
             # A good diagnostic for conv nets is often whether they are learning
             # nice filters in their first layer. This is because it tells us
@@ -78,7 +73,7 @@ for i in range(20):
             print "Activations (vertical streaks are bad):"
             # A big of an ugly cludge to visualize each matrix entry with one char
             # Normaly visualize as 2 char or a half char, to avoid rectangular
-            vis.vis_matrix(np.repeat(info["y"], 2, axis = 0)[:15, :200], s2x2 = True)
+            vis.vis_matrix(np.repeat(info["y"], 2, axis = 0)[:30, :200], s2x2 = True)
     
 
     print 80*"#"
